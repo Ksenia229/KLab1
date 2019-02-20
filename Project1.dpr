46f5e8bdec3e87c0c83887dbe4547235be8e4bd8
Ksenia229 <47811158+Ksenia229@users.noreply.github.com>
2019-02-20 11:18:25 +0300

    DIO

---
+++ Project1.dpr
@@ -0,0 +1,17 @@
+program Project1;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  Unit1 in 'Unit1.pas' {Form1};
+
+begin
+  { TODO -oUser -cConsole Main : Insert code here }
+  Writeln('Hello, World from Delphi!');
+  Writeln('This is the console application');
+  Writeln;
+  Writeln('Press <Enter> to quit.');
+  Readln;
+
+end.
---
+++ Unit1.pas
@@ -0,0 +1,27 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    RadioButton1: TRadioButton;
+    Button1: TButton;
+    GroupBox1: TGroupBox;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
